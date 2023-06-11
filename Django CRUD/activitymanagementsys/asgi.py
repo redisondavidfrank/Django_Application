184d02210a9d1ce5aa38f921494705fb14fee72d
redisondavidfrank <121031464+redisondavidfrank@users.noreply.github.com>
2023-06-11 11:44:27 +0800

    Add files via upload

---
+++ Django CRUD/activitymanagementsys/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for activitymanagementsys project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'activitymanagementsys.settings')
+
+application = get_asgi_application()
---
+++ Django CRUD/djangocrudactivity/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangocrudactivityConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'djangocrudactivity'
